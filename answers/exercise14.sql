2607d732a5c4a79e818216e7ff318b919cf12c7c
Gabrielle Damazio <damaziogabrielle@gmail.com>
2024-11-14 19:44:15 -0500

    Update exercise14.sql

---
+++ answers/exercise14.sql
@@ -0,0 +1,16 @@
+mysql> describe Users;
++------------+--------------+------+-----+---------+-------+
+| Field      | Type         | Null | Key | Default | Extra |
++------------+--------------+------+-----+---------+-------+
+| UserID     | int          | YES  |     | NULL    |       |
+| Last_Name  | varchar(255) | YES  |     | NULL    |       |
+| First_Name | varchar(255) | YES  |     | NULL    |       |
+| Address    | varchar(255) | YES  |     | NULL    |       |
+| City       | varchar(255) | YES  |     | NULL    |       |
+| Initial    | varchar(64)  | YES  |     | NULL    |       |
+| Email      | varchar(255) | YES  |     | NULL    |       |
+| BadgeID    | varchar(255) | YES  |     | NULL    |       |
++------------+--------------+------+-----+---------+-------+
+9 rows in set (0.00 sec)
+
+mysql> 
