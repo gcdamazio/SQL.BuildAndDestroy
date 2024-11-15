da6cde045b42a78e2b8b3eabf24d3f73b2272103
Gabrielle Damazio <damaziogabrielle@gmail.com>
2024-11-14 19:12:33 -0500

    Update exercise8.sql

--- answers/exercise8.sql
+++ answers/exercise8.sql
@@ -1,4 +1,4 @@
-mysql> ALTER TABLE mynewDB.users
-    -> ADD Birthday DATE;
+> ALTER TABLE mynewDB.users
+-> ADD Birthday DATE;
 Query OK, 0 rows affected (0.04 sec)
 Records: 0  Duplicates: 0  Warnings: 0
