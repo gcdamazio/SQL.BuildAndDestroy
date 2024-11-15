4ec68637ab2d44c6326c3f680500f1539f8bd915
Gabrielle Damazio <damaziogabrielle@gmail.com>
2024-11-14 19:25:47 -0500

    Update exercise10.sql

--- answers/exercise10.sql
+++ answers/exercise10.sql
@@ -1,6 +1,4 @@
-ALTER TABLE Users
-ADD MiddleName VARCHAR(255),
-    NickName VARCHAR(255),
-    Email VARCHAR(255),
-    Suffix VARCHAR(64),
-    BadgeID VARCHAR(255);
+ALTER TABLE Users ADD MiddleName VARCHAR(255),ADD NickName VARCHAR(255),
+    ADD Email VARCHAR(255),ADD Suffix VARCHAR(64),ADD BadgeID VARCHAR(255);
+Query OK, 0 rows affected (0.03 sec)
+Records: 0  Duplicates: 0  Warnings: 0
